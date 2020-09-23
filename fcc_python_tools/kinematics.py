898405c064b47aa469554928343b7dc6c96992e5
donalrinho <donal.hill@cern.ch>
2020-09-23 23:17:51 +0200

    Added phi and eta functions.

--- fcc_python_tools/kinematics.py
+++ fcc_python_tools/kinematics.py
@@ -9,6 +9,14 @@ def calc_p(array, container):
 def calc_pt(array, container):
     return np.sqrt(array[container,'p4.px']**2 + array[container,'p4.py']**2)
 
+#Pseudorapidity
+def calc_eta(array, container):
+    return np.arcsinh(array[container,'p4.pz'] / calc_pt(array, container))
+
+#Phi
+def calc_phi(array, container):
+    return np.arccos(array[container,'p4.px'] / calc_pt(array, container))
+
 #Invariant mass of 2 particles
 def mass_2body(left, right, left_mass, right_mass):
     left_energy = np.sqrt(left['p4.p']**2 + left_mass**2)
