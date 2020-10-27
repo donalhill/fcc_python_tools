1384fdc8e5f8ed158e9616e9f884bbd0e6853b43
Donal Hill <donal.hill@cern.ch>
2020-10-27 23:00:51 +0100

    Added B -> D0 pi with exclusive sample.

--- fcc_python_tools/kinematics.py
+++ fcc_python_tools/kinematics.py
@@ -2,41 +2,62 @@ import awkward1 as ak
 import numpy as np
 
 #Momentum
-def calc_p(array, container):
-    return np.sqrt(array[container,'momentum.x']**2 + array[container,'momentum.y']**2 + array[container,'momentum.z']**2)
+def calc_p(array, container, isFCCEDM=False):
+    if(isFCCEDM):
+        prefix = 'p4.p'
+    else:
+        prefix = 'momentum.'
+    return np.sqrt(array[container,f'{prefix}x']**2 + array[container,f'{prefix}y']**2 + array[container,f'{prefix}z']**2)
 
 #Transverse momentum
-def calc_pt(array, container):
-    return np.sqrt(array[container,'momentum.x']**2 + array[container,'momentum.y']**2)
+def calc_pt(array, container, isFCCEDM=False):
+    if(isFCCEDM):
+        prefix = 'p4.p'
+    else:
+        prefix = 'momentum.'
+    return np.sqrt(array[container,f'{prefix}x']**2 + array[container,f'{prefix}y']**2)
 
 #Pseudorapidity
-def calc_eta(array, container):
-    return np.arcsinh(array[container,'momentum.z'] / calc_pt(array, container))
+def calc_eta(array, container, isFCCEDM=False):
+    if(isFCCEDM):
+        prefix = 'p4.p'
+    else:
+        prefix = 'momentum.'
+    return np.arcsinh(array[container,f'{prefix}z'] / calc_pt(array, container, isFCCEDM))
 
 #Theta
-def calc_theta(array, container):
-    eta = calc_eta(array,container)
+def calc_theta(array, container, isFCCEDM=False):
+    eta = calc_eta(array,container, isFCCEDM)
     return 2 * np.arctan(np.exp(-eta))
 
 #Phi
-def calc_phi(array, container):
-    return np.arccos(array[container,'momentum.x'] / calc_pt(array, container))
+def calc_phi(array, container, isFCCEDM=False):
+    if(isFCCEDM):
+        prefix = 'p4.p'
+    else:
+        prefix = 'momentum.'
+    return np.arccos(array[container,f'{prefix}x'] / calc_pt(array, container, isFCCEDM))
 
 #Invariant mass for a list of particles, given a list of corresponding rest masses for each particle
-def mass(particles, masses):
+def mass(particles, masses, isFCCEDM=False):
+    if(isFCCEDM):
+        prefix = 'p4.p'
+    else:
+        prefix = 'momentum.'
+
     for i in range(0,len(particles)):
         particles[i]['e'] = np.sqrt(particles[i]['p']**2 + masses[i]**2)
 
     tot_energy = particles[0]['e']
-    tot_px = particles[0]['momentum.x']
-    tot_py = particles[0]['momentum.y']
-    tot_pz = particles[0]['momentum.z']
+    tot_px = particles[0][f'{prefix}x']
+    tot_py = particles[0][f'{prefix}y']
+    tot_pz = particles[0][f'{prefix}z']
 
     for i in range(1,len(particles)):
         tot_energy = tot_energy + particles[i]['e']
-        tot_px = tot_px + particles[i]['momentum.x']
-        tot_py = tot_py + particles[i]['momentum.y']
-        tot_pz = tot_pz + particles[i]['momentum.z']
+        tot_px = tot_px + particles[i][f'{prefix}x']
+        tot_py = tot_py + particles[i][f'{prefix}y']
+        tot_pz = tot_pz + particles[i][f'{prefix}z']
 
     return np.sqrt(tot_energy**2 - tot_px**2 - tot_py**2 - tot_pz**2)
 
